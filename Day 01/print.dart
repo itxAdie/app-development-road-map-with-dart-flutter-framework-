3f48fff10a5647c82d52b2dd5d8c035a4b3aa833
itxAdie <itxadiert@gmail.com>
2024-06-24 10:02:28 -0700

    day one of learning dart

---
+++ Day 01/print.dart
@@ -0,0 +1,41 @@
+void main() {
+  // writing style
+  // three types of writing
+
+  // print('Adeel');
+
+  // print('Welcome to dart learning.');
+
+  // print(
+  //     "Welcome to class, we're dart learning.\nWelcome to class, we're dart learning.\nWelcome to class, we're dart learning.\nWelcome to class, we're dart learning.");
+
+//   print("Welcome
+// to
+// class,
+// we're
+// dart
+// learning.");
+
+  print('''Welcome
+to
+class,
+we're
+dart
+learning.
+to
+class,
+we're
+dart
+learning.
+to
+class,
+we're
+dart
+learning.
+to
+class,
+we're
+dart
+learning.
+''');
+}
---
+++ Day 01/variables.dart
@@ -0,0 +1,43 @@
+void main() {
+  // <datatype> <variableName> = value;
+
+  /*
+  int,
+  double,
+  String,
+  bool,
+  dynamic
+  */
+
+  int firstNumber = 12;
+
+  print(firstNumber);
+
+  double firstFlotingPoint = 10.10;
+
+  print(firstFlotingPoint);
+
+  String myName = 'Adeel';
+
+  print(myName);
+
+  bool isClassGoingOn = true;
+
+  print(isClassGoingOn);
+
+  dynamic dynamicInt = 12;
+
+  print(dynamicInt.runtimeType);
+
+  dynamic dynamicString = 'Adeel';
+
+  print(dynamicString.runtimeType);
+
+  dynamic dynamicBool = false;
+
+  print(dynamicBool.runtimeType);
+
+  dynamic dynamicFlotingPoint = 12.0;
+
+  print(dynamicFlotingPoint.runtimeType);
+}
